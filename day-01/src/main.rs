c04c912a56a7ef9669f324a96baea8d709a0f9d7
kinokasai <>
2025-12-01 14:11:01 +0100

    second star

--- day-01/src/main.rs
+++ day-01/src/main.rs
@@ -14,8 +14,8 @@ fn parse(filename: &str) -> Vec<Command> {
     let value = s.chars().skip(1).collect::<String>();
     let value = str::parse::<i32>(&value).unwrap();
     match idx {
-      Some('L') => Command::Sub(value % 100),
-      Some('R') => Command::Add(value % 100),
+      Some('L') => Command::Sub(value),
+      Some('R') => Command::Add(value),
       _ => panic!("this should not happen")
     }
     }).collect::<Vec<Command>>()
@@ -25,11 +25,24 @@ fn execute(commands: Vec<Command>) -> i32 {
   let mut counter = 0;
   commands.into_iter().fold(50, |acc, command| {
     let acc = match command {
-      Command::Add(i) => (acc + i) % 100,
-      Command::Sub(i) => if acc - i < 0 { acc + 100 - i } else { acc - i }
+      Command::Add(i) => {
+        let total = acc + i;
+        let rem = total % 100;
+        let quotient = total / 100;
+        counter += quotient;
+        rem
+      },
+      Command::Sub(i) => {
+        // We don't want to count 0 -> [xx] as another passage through 0.
+        if acc == 0 {counter -= 1;}
+        let total = (100 - acc) + i;
+        let quotient = total / 100;
+        counter += quotient;
+        let rem = (100 - (total % 100)) % 100;
+        rem
+      }
     };
-    println!("{:?} -> {acc}!", command);
-    if acc == 0 || acc == 100 { counter += 1; };
+    println!("{:?} -> {acc} | {counter}", command);
     acc
   });
   counter
