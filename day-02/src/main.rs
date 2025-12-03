02f93391dad00a65bc59f136ccbc3daf480a3ca0
kinokasai <>
2025-12-03 10:55:09 +0100

    day-02 second star

--- day-02/src/main.rs
+++ day-02/src/main.rs
@@ -13,7 +13,7 @@ fn parse(filename: &str) -> Vec<Range> {
     .split(",")
     .map(|s| s.split("-").collect::<Vec<&str>>())
     .map(|arr| {
-      println!("{:?}", arr);
+      // println!("{:?}", arr);
       Range {
         low: str::parse::<i64>(arr[0]).unwrap(),
         high: str::parse::<i64>(arr[1]).unwrap(),
@@ -28,28 +28,38 @@ fn digits(n: i64) -> u32 {
 }
 
 fn count_invalid(range: &Range) -> i64 {
+  // println!("range: {:?}", range);
   let mut counter = 0;
   for n in range.low..=range.high {
-    let digits = digits(n);
-    // println!("{digits}");
-    if digits % 2 != 0 {
-      continue;
+    let digits = digits(n) as usize;
+    // println!("processing: {n} | digits: {digits}");
+    let str = n.to_string().chars().collect::<Vec<char>>();
+    for i in (1..=digits/2).rev() {
+      let chunks = str
+        .chunks(i)
+        .map(|c| c.iter().collect::<String>())
+        .collect::<Vec<String>>();
+      // println!("chunks: {:?}", &chunks);
+      let goal = &chunks[0];
+      let equal = chunks.iter().fold(true, |acc, e| acc && e == goal);
+      if equal {
+        // println!("bad id: {n}");
+        counter += n;
+        break;
+      }
     }
-    let trunc_idx = 10i64.pow(digits / 2);
-    let left = n / trunc_idx;
-    let right = n % trunc_idx;
-    if left == right {
-      counter += n;
-    }
-    // println!("left: {left} | right: {right}");
   }
   counter
 }
 
 fn main() {
   let ranges = parse(&args().nth(1).unwrap());
-  // let counter = count_invalid(&ranges[0]);
-  let counter = ranges.iter().map(|range| count_invalid(range)).reduce(|a,b| a + b).unwrap_or(0);
-  println!("{counter}");
+  // let counter = count_invalid(&ranges[2]);
+  let counter = ranges
+    .iter()
+    .map(|range| count_invalid(range))
+    .reduce(|a, b| a + b)
+    .unwrap_or(0);
+  println!("counter: {counter}");
   // println!("{:?}", ranges);
 }
